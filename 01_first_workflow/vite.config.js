30ad0600a8c96f172555ace10c9a60dc449a340c
jorgescalona <escajorge@gmail.com>
2025-12-08 10:33:07 -0300

    [REF] gh-actions: change vite config js file

--- 01_first_workflow/vite.config.js
+++ 01_first_workflow/vite.config.js
@@ -1,1 +1,13 @@
-// ...vite.config.js content...
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react(), eslint()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './src/test/setup.js',
+	},
+});
