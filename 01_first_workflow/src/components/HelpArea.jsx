5b0098bfad5f6309b7bfce4ddb325fdfe9173dc7
jorgescalona <escajorge@gmail.com>
2025-12-08 10:45:07 -0300

    [REF] gh-actions: Created the missing HelpArea.jsx component with the required data-testid=help-area attribute

---
+++ 01_first_workflow/src/components/HelpArea.jsx
@@ -0,0 +1,11 @@
+import './HelpArea.css';
+
+function HelpArea() {
+	return (
+		<div data-testid="help-area" className="help-area">
+			<p>This is the help area with useful information.</p>
+		</div>
+	);
+}
+
+export default HelpArea;
